c62f070d66e0d40fe0772affdabb7bd4318e71e8
detrna <himselfunderscore@gmail.com>
2026-04-01 14:27:54 +0700

    socket.io practice files

--- socket-io/socket-io.js
+++ socket-io/socket-io.js
@@ -1,5 +1,3 @@
-import pool from "./pool.js";
-
 export default function setupSocket(io) {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
@@ -14,14 +12,10 @@ export default function setupSocket(io) {
     socket.on("send_message", (data) => {
       console.log(data);
       io.emit("receive_message", data);
-      // If Postman sends { "roomId": "1" },
-      // you MUST use data.roomId here.
+
       const targetRoom = data.roomId;
 
-      console.log("Sending to room:", targetRoom);
-
       const roomSize = io.sockets.adapter.rooms.get(targetRoom)?.size || 0;
-      console.log(`Users in room: ${roomSize}`);
 
       io.to(targetRoom).emit("receive_message", data);
     });
